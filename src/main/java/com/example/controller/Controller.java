4a3e25a3a779aed49d4b756a64101de62a91ed37
susanta <ssbarmant107@gmail.com>
2023-11-22 03:18:12 -1200

    v1--> added test project for jenkin test

---
+++ src/main/java/com/example/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.entity.Book;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@GetMapping("/getAll")
+	public String getBookData(Model model)
+	{
+		Book b=new Book(1,"java",45.90);
+		Book b1=new Book(3,"sql",45.90);
+		Book b2=new Book(2,"spring",45.90);
+		Book b3=new Book(4,"c++",45.90);
+		List<Book> bl=new ArrayList<>();
+		bl.add(b3);
+		bl.add(b2);
+		bl.add(b);
+		bl.add(b1);
+		model.addAttribute("bl", bl);
+		return "search";
+	}
+}
